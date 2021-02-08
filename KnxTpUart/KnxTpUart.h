518e3186b7c9f4b1b54045ffb7f7e1a71306ebd4
Michael Weiss <michael.weiss@bayer.com>
2021-02-08 10:11:07 +0100

    Comments added

--- KnxTpUart/KnxTpUart.h
+++ KnxTpUart/KnxTpUart.h
@@ -115,26 +115,165 @@ class KnxTpUart {
 
     /**
      * Send a boolean (1bit) value to a group address.
+     * This can be used for DPT-1.
      * @param aAddress the address to write to.
      * @param aValue the value to write.
      * @return true if writing was successful, false otherwise.
+     * @see #groupAnswerBool
      */
     bool groupWriteBool(String aAddress, bool aValue);
+
+    /**
+     * Send a boolean (1bit) value to a group address.
+     * This can be used for DPT-1.
+     * @param aAddress the address to write to.
+     * @param aValue the value to write.
+     * @return true if writing was successful, false otherwise.
+     * @see #groupAnswerBool
+     */
     bool groupWriteBool(uint16_t aAddress, bool aValue);
 
+    /**
+     * Send a 4bit value to a group address.
+     * This can be used for DPT-2 or DPT-3.
+     * @param aAddress the address to write to.
+     * @param aValue the value to write.
+     * @return true if writing was successful, false otherwise.
+     * @see #groupAnswer4BitInt
+     */
     bool groupWrite4BitInt(String aAddress, int8_t aValue);
+
+    /**
+     * Send a 4bit value to a group address.
+     * This can be used for DPT-2 or DPT-3.
+     * @param aAddress the address to write to.
+     * @param aValue the value to write.
+     * @return true if writing was successful, false otherwise.
+     * @see #groupAnswer4BitInt
+     */
     bool groupWrite4BitInt(uint16_t aAddress, int8_t aValue);
 
+    /**
+     * Send a boolean + 3 bit value (DPT-3) to a group address.
+     * @param aAddress the address to write to.
+     * @param aDirection (1vit value).
+     * @param aSteps (3bit value)
+     * @return true if writing was successful, false otherwise.
+     * @see #groupAnswer4BitDim
+     */
     bool groupWrite4BitDim(String aAddress, bool aDirection, byte aSteps);
+
+    /**
+     * Send a boolean + 3 bit value (DPT-3) to a group address.
+     * @param aAddress the address to write to.
+     * @param aDirection (1vit value).
+     * @param aSteps (3bit value)
+     * @return true if writing was successful, false otherwise.
+     * @see #groupAnswer4BitDim
+     */
     bool groupWrite4BitDim(uint16_t aAddress, bool aDirection, byte aSteps);
 
+    /**
+	 * Send a 8bit signed integer value to a group address.
+	 * @param aAddress the address to write to.
+	 * @param aValue the value to send.
+	 * @return true if writing was successful, false otherwise.
+	 * @see #groupAnswer1ByteInt
+	 */
     bool groupWrite1ByteInt(String aAddress, int8_t aValue);
+
+    /**
+	 * Send a 8bit signed integer value to a group address.
+	 * @param aAddress the address to write to.
+	 * @param aValue the value to send.
+	 * @return true if writing was successful, false otherwise.
+	 * @see #groupAnswer1ByteInt
+	 */
     bool groupWrite1ByteInt(uint16_t aAddress, int8_t aValue);
 
+    /**
+	 * Send a 8bit unsigned integer value to a group address.
+	 * @param aAddress the address to write to.
+	 * @param aValue the value to send.
+	 * @return true if writing was successful, false otherwise.
+	 * @see #groupAnswer1ByteUInt
+	 */
+    bool groupWrite1ByteUInt(String aAddress, uint8_t aValue);
+
+    /**
+	 * Send a 8bit unsigned integer value to a group address.
+	 * @param aAddress the address to write to.
+	 * @param aValue the value to send.
+	 * @return true if writing was successful, false otherwise.
+	 * @see #groupAnswer1ByteUInt
+	 */
+    bool groupWrite1ByteUInt(uint16_t aAddress, uint8_t aValue);
+
+    /**
+	 * Send a 16bit signed integer value to a group address.
+	 * @param aAddress the address to write to.
+	 * @param aValue the value to send.
+	 * @return true if writing was successful, false otherwise.
+	 * @see #groupAnswer2ByteUInt
+	 */
     bool groupWrite2ByteInt(String aAddress, int16_t aValue);
+
+    /**
+	 * Send a 16bit signed integer value to a group address.
+	 * @param aAddress the address to write to.
+	 * @param aValue the value to send.
+	 * @return true if writing was successful, false otherwise.
+	 * @see #groupAnswer2ByteInt
+	 */
     bool groupWrite2ByteInt(uint16_t aAddress, int16_t aValue);
 
+    /**
+	 * Send a 16bit unsigned integer value to a group address.
+	 * @param aAddress the address to write to.
+	 * @param aValue the value to send.
+	 * @return true if writing was successful, false otherwise.
+	 */
+    bool groupWrite2ByteUInt(String aAddress, uint16_t aValue);
+
+    /**
+	 * Send a 16bit unsigned integer value to a group address.
+	 * @param aAddress the address to write to.
+	 * @param aValue the value to send.
+	 * @return true if writing was successful, false otherwise.
+	 */
+    bool groupWrite2ByteUInt(uint16_t aAddress, uint16_t aValue);
+
+    /**
+	 * Send a 32bit signed integer value to a group address.
+	 * @param aAddress the address to write to.
+	 * @param aValue the value to send.
+	 * @return true if writing was successful, false otherwise.
+	 * @see #groupAnswer4ByteInt
+	 */
+    bool groupWrite4ByteInt(uint16_t aAddress, int32_t aValue);
+    /**
+	 * Send a 32bit unsigned integer value to a group address.
+	 * @param aAddress the address to write to.
+	 * @param aValue the value to send.
+	 * @return true if writing was successful, false otherwise.
+	 * @see #groupAnswer4ByteUInt
+	 */
+    bool groupWrite4ByteUInt(uint16_t aAddress, uint32_t aValue);
+
+    /**
+	 * Send a 16bit float value to a group address.
+	 * @param aAddress the address to write to.
+	 * @param aValue the value to send.
+	 * @return true if writing was successful, false otherwise.
+	 */
     bool groupWrite2ByteFloat(String aAddress, float aValue);
+
+    /**
+	 * Send a 16bit float value to a group address.
+	 * @param aAddress the address to write to.
+	 * @param aValue the value to send.
+	 * @return true if writing was successful, false otherwise.
+	 */
     bool groupWrite2ByteFloat(uint16_t aAddress, float aValue);
 
     bool groupWrite3ByteTime(String aAddress, uint8_t aWeekday, uint8_t aHour, uint8_t aMinute, uint8_t aSecond);
@@ -143,30 +282,200 @@ class KnxTpUart {
     bool groupWrite3ByteDate(String aAddress, uint8_t aDay, uint8_t aMonth, uint8_t aYear);
     bool groupWrite3ByteDate(uint16_t aAddress, uint8_t aDay, uint8_t aMonth, uint8_t aYear);
 
+    /**
+	 * Send a 32bit float value to a group address.
+	 * @param aAddress the address to write to.
+	 * @param aValue the value to send.
+	 * @return true if writing was successful, false otherwise.
+	 */
     bool groupWrite4ByteFloat(String aAddress, float aValue);
+    /**
+	 * Send a 32bit float value to a group address.
+	 * @param aAddress the address to write to.
+	 * @param aValue the value to send.
+	 * @return true if writing was successful, false otherwise.
+	 */
     bool groupWrite4ByteFloat(uint16_t aAddress, float aValue);
 
+    /**
+	 * Send a 14 byte text value to a group address.
+	 * @param aAddress the address to write to.
+	 * @param aValue the value to send.
+	 * @return true if writing was successful, false otherwise.
+	 */
     bool groupWrite14ByteText(String aAddress, String aValue);
+
+    /**
+	 * Send a 14 byte text value to a group address.
+	 * @param aAddress the address to write to.
+	 * @param aValue the value to send.
+	 * @return true if writing was successful, false otherwise.
+	 */
     bool groupWrite14ByteText(uint16_t aAddress, String aValue);
 
+    /**
+	 * Send a boolean (1bit) value to a group address.
+	 * This can be used for DPT-1.
+	 * @param aAddress the address to write to.
+	 * @param aValue the value to write.
+	 * @return true if writing was successful, false otherwise.
+	 * @see #groupWriteBool
+	 */
     bool groupAnswerBool(String aAddress, bool aValue);
+
+    /**
+	 * Send a boolean (1bit) answer to a group address.
+	 * This can be used for DPT-1.
+	 * @param aAddress the address to write to.
+	 * @param aValue the value to write.
+	 * @return true if writing was successful, false otherwise.
+	 * @see #groupWriteBool
+	 */
     bool groupAnswerBool(uint16_t aAddress, bool aValue);
 
-
+    /**
+     * Send a 4bit answer to a group address.
+     * This can be used for DPT-2 or DPT-3.
+     * @param aAddress the address to write to.
+     * @param aValue the value to write.
+     * @return true if writing was successful, false otherwise.
+     * @see #groupWrite4BitInt
+     */
     bool groupAnswer4BitInt(String, int8_t aValue);
+
+    /**
+     * Send a 4bit answer to a group address.
+     * This can be used for DPT-2 or DPT-3.
+     * @param aAddress the address to write to.
+     * @param aValue the value to write.
+     * @return true if writing was successful, false otherwise.
+     * @see #groupWrite4BitInt
+     */
     bool groupAnswer4BitInt(uint16_t, int8_t aValue);
 
+    /**
+     * Send a boolean + 3 bit answer (DPT-3) to a group address.
+     * @param aAddress the address to write to.
+     * @param aDirection (1vit value).
+     * @param aSteps (3bit value)
+     * @return true if writing was successful, false otherwise.
+     * @see #groupWrite4BitDim
+     */
     bool groupAnswer4BitDim(String, bool aDirection, byte aSteps);
+
+    /**
+     * Send a boolean + 3 bit answer (DPT-3) to a group address.
+     * @param aAddress the address to write to.
+     * @param aDirection (1vit value).
+     * @param aSteps (3bit value)
+     * @return true if writing was successful, false otherwise.
+     * @see #groupWrite4BitDim
+     */
     bool groupAnswer4BitDim(uint16_t, bool aDirection, byte aSteps);
 
-
+    /**
+	 * Send a 8bit signed integer answer to a group address.
+	 * @param aAddress the address to write to.
+	 * @param aValue the value to send.
+	 * @return true if writing was successful, false otherwise.
+	 * @see #groupWrite1ByteInt
+	 */
     bool groupAnswer1ByteInt(String aAddress, int8_t aValue);
+
+    /**
+	 * Send a 8bit signed integer answer to a group address.
+	 * @param aAddress the address to write to.
+	 * @param aValue the value to send.
+	 * @return true if writing was successful, false otherwise.
+	 * @see #groupWrite1ByteInt
+	 */
     bool groupAnswer1ByteInt(uint16_t aAddress, int8_t aValue);
 
+    /**
+	 * Send a 8bit unsigned integer answer to a group address.
+	 * @param aAddress the address to write to.
+	 * @param aValue the value to send.
+	 * @return true if writing was successful, false otherwise.
+	 * @see #groupWrite1ByteUInt
+	 */
+    bool groupAnswer1ByteUInt(String aAddress, uint8_t aValue);
+    /**
+	 * Send a 8bit unsigned integer answer to a group address.
+	 * @param aAddress the address to write to.
+	 * @param aValue the value to send.
+	 * @return true if writing was successful, false otherwise.
+	 * @see #groupWrite1ByteUInt
+	 */
+    bool groupAnswer1ByteUInt(uint16_t aAddress, uint8_t aValue);
+
+    /**
+	 * Send a 16bit signed integer answer to a group address.
+	 * @param aAddress the address to write to.
+	 * @param aValue the value to send.
+	 * @return true if writing was successful, false otherwise.
+	 * @see #groupWrite2ByteInt
+	 */
     bool groupAnswer2ByteInt(String aAddress, int16_t aValue);
+    /**
+	 * Send a 16bit signed integer answer to a group address.
+	 * @param aAddress the address to write to.
+	 * @param aValue the value to send.
+	 * @return true if writing was successful, false otherwise.
+	 * @see #groupWrite2ByteInt
+	 */
     bool groupAnswer2ByteInt(uint16_t aAddress, int16_t aValue);
 
+    /**
+	 * Send a 16bit unsigned integer answer to a group address.
+	 * @param aAddress the address to write to.
+	 * @param aValue the value to send.
+	 * @return true if writing was successful, false otherwise.
+	 * @see #groupWrite2ByteUInt
+	 */
+    bool groupAnswer2ByteUInt(String aAddress, uint16_t aValue);
+
+    /**
+	 * Send a 16bit unsigned integer answer to a group address.
+	 * @param aAddress the address to write to.
+	 * @param aValue the value to send.
+	 * @return true if writing was successful, false otherwise.
+	 * @see #groupWrite2ByteUInt
+	 */
+    bool groupAnswer2ByteUInt(uint16_t aAddress, uint16_t aValue);
+
+    /**
+	 * Send a 32bit signed integer answer to a group address.
+	 * @param aAddress the address to write to.
+	 * @param aValue the value to send.
+	 * @return true if writing was successful, false otherwise.
+	 * @see #groupWrite2ByteInt
+	 */
+    bool groupAnswer4ByteInt(uint16_t aAddress, int32_t aValue);
+
+    /**
+	 * Send a 32bit unsigned integer answer to a group address.
+	 * @param aAddress the address to write to.
+	 * @param aValue the value to send.
+	 * @return true if writing was successful, false otherwise.
+	 * @see #groupWrite4ByteUInt
+	 */
+    bool groupAnswer4ByteUInt(uint16_t aAddress, uint32_t aValue);
+
+    /**
+	 * Send a 16bit float answer to a group address.
+	 * @param aAddress the address to write to.
+	 * @param aValue the value to send.
+	 * @return true if writing was successful, false otherwise.
+	 * @see #groupWrite2ByteFloat
+	 */
     bool groupAnswer2ByteFloat(String aAddress, float aValue);
+    /**
+	 * Send a 16bit float answer to a group address.
+	 * @param aAddress the address to write to.
+	 * @param aValue the value to send.
+	 * @return true if writing was successful, false otherwise.
+	 * @see #groupWrite2ByteFloat
+	 */
     bool groupAnswer2ByteFloat(uint16_t aAddress, float aValue);
 
     bool groupAnswer3ByteTime(String aAddress, uint8_t aWeekday, uint8_t aHour, uint8_t aMinute, uint8_t aSecond);
@@ -175,7 +484,22 @@ class KnxTpUart {
     bool groupAnswer3ByteDate(String aAddress, uint8_t aDay, uint8_t aMonth, uint8_t aYear);
     bool groupAnswer3ByteDate(uint16_t aAddress, uint8_t aDay, uint8_t aMonth, uint8_t aYear);
 
+    /**
+	 * Send a 32bit float answer to a group address.
+	 * @param aAddress the address to write to.
+	 * @param aValue the value to send.
+	 * @return true if writing was successful, false otherwise.
+	 * @see #groupWrite4ByteFloat
+	 */
     bool groupAnswer4ByteFloat(String aAddress, float aValue);
+
+    /**
+	 * Send a 32bit float answer to a group address.
+	 * @param aAddress the address to write to.
+	 * @param aValue the value to send.
+	 * @return true if writing was successful, false otherwise.
+	 * @see #groupWrite4ByteFloat
+	 */
     bool groupAnswer4ByteFloat(uint16_t aAddress, float aValue);
 
     bool groupAnswer14ByteText(String aAddress, String aValue);
@@ -216,9 +540,6 @@ class KnxTpUart {
 	 * @return the two byte address.
 	 */
     uint16_t getSourceAddress(String aAddress);
-
-
-    uint16_t parseAddress(String aAddress, const char aDelimiter);
 
 
   private:
